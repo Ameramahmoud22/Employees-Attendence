c67313a29c19d4f05e6d1cfaaad6303fb9a6839f
Amera <ameraelbassal552005@gmail.com>
2025-10-31 16:36:20 +0200

    add worker model

---
+++ Employees-Attendence/Models/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace Employees_Attendence.Models
+{
+    public class Worker
+    {
+      
+        
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "اسم العامل مطلوب")]
+            [Display(Name = "الاسم")]
+            public string Name { get; set; }
+
+            [Display(Name = "نوع العامل")]
+            public string Type { get; set; } // "عامل" أو "صنايعي" مثلاً
+
+            [Display(Name = "الهاتف")]
+            public string Phone { get; set; }
+
+            [Display(Name = "العنوان")]
+            public string Address { get; set; }
+
+            [Display(Name = "مبلغ القبض باليوم")]
+            [Column(TypeName = "decimal(18,2)")]
+            public decimal DailyWage { get; set; } = 0m;
+
+            [Display(Name = "ملاحظات")]
+            public string Notes { get; set; }
+
+            [Display(Name = "الفئة")]
+            public int CategoryId { get; set; }
+            public Category Category { get; set; }
+        }
+    }
+
