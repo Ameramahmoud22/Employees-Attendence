6dbe01e020caccd6c64796a141548a15ccc5b50d
Amera <ameraelbassal552005@gmail.com>
2025-10-31 16:42:07 +0200

    add all

--- Employees-Attendence/Models/Category.cs
+++ Employees-Attendence/Models/Category.cs
@@ -1,6 +1,17 @@
-﻿namespace Employees_Attendence.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employees_Attendence.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "اسم الفئة مطلوب")]
+        [Display(Name = "اسم الفئة")]
+        public string Name { get; set; }
+
+        public ICollection<Worker> Workers { get; set; }
+
     }
 }
